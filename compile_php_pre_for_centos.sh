6a1c7896dedb810c4c83b274f85a3792951dc1ed
meast <pzd2006@126.com>
2018-06-01 13:35:15 +0800

    Update compile_php_pre_for_centos.sh

--- compile_php_pre_for_centos.sh
+++ compile_php_pre_for_centos.sh
@@ -1,2 +1,2 @@
-yum install libjpeg-devel giflib-devel libpng-devel libXpm-devel gettext-devel gd-devel freetype-devel zlib-devel tinyxml-devel t1lib-devel openssl-devel libiconv-devel libxml2-devel bison libicu-devel sqlite-devel autoconf libcurl-devel bzip2-devel libmcrypt-devel re2c re2c-devel uw-imap-devel uw-imap-utils uw-imap 
+yum install libjpeg-devel giflib-devel libpng-devel libXpm-devel gettext-devel gd-devel freetype-devel zlib-devel tinyxml-devel t1lib-devel openssl-devel libiconv-devel libxml2-devel bison libicu-devel sqlite-devel autoconf libcurl-devel bzip2-devel libmcrypt-devel re2c re2c-devel uw-imap-devel uw-imap-utils uw-imap libc-client 
 
